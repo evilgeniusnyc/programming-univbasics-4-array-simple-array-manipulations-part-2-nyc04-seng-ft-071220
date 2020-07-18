da4823a0e2ef5f7b182f88c442940276edb1ce8b
Steven Williams <evilgeniusnyc@me.com>
2020-07-18 20:14:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,10 @@ def using_concat(array1, array2)
   array1.concat (array2)
 end
 
-def using_insert(array, element)
-  array = [ ]
-  insert (4)
+def using_insert(array, item)
+  array.insert(4, item)
 end
   
+def using_uniq(array,duplicate)
+  array.uniq
+end
