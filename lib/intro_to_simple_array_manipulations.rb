92003c22793f793bb99f52fb113e8cd097f29037
Steven Williams <evilgeniusnyc@me.com>
2020-07-18 20:19:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,10 +15,9 @@ def using_flatten(array)
 end
   
 def using_delete(array, string)
-  
+  array.delete(string)
 end
 
-def using_delete_at(array, interger)
-  
-end 
-
+def using_delete_at(array, num)
+  array.delete_at(num)
+end
